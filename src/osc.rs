63e39b2324cac031e6a1001285dedec01c53e954
Kevin Waddle <bonzobavaria@gmail.com>
2020-03-28 16:39:27 -0500

    Add memoization to OscReader

--- src/osc.rs
+++ src/osc.rs
@@ -2,55 +2,49 @@
 // it reads from. It only tracks the table position to read from, and takes a
 // reference to a vector to read from as an argument to the read function.
 
+// Note that phase is public. This leaves callers or higher-level modules free
+// to perform modulation or sync in any way they see fit.
 #[derive(Clone)]
 pub struct OscReader {
-    pub active: bool,
-    pub wt_increment: f32,
-    pub wt_position: f32,
-    pub frequency: f32,
-    pub sample_rate: u32,
-    table_size: usize,
+    pub phase: f32,
+    memo: Memo,
+}
+
+#[derive(Clone)]
+struct Memo {
+    frequency: f32,
+    sample_rate: u32,
+    phase_inc: f32,
 }
 
 impl OscReader {
-    pub fn new(
-        table_size: usize, 
-        sample_rate: u32,
-    ) -> OscReader {
-        OscReader {
-            active: false,
-            frequency: 0.0,
-            sample_rate,
-            table_size,
-            wt_increment: 0.0,
-            wt_position: 0.0,
+    pub fn new() -> OscReader {
+        OscReader { 
+            phase: 0.0, 
+            memo: Memo {
+                frequency: 0.0,
+                sample_rate: 44100,
+                phase_inc: 0.0,
+            }
         }
     }
     pub fn read(&self, table: &Vec<f32>) -> f32 {
-        table[self.wt_position as usize]
+        table[(self.phase * table.len() as f32) as usize]
     }
-    pub fn increment(&mut self) {
-        self.wt_position += self.wt_increment;
-        self.wt_position %= self.table_size as f32;
-    }
-    // TODO: Place reponsibiliy for calculating this on the caller directly or
-    // indirectly by using a wrapper with memoization capabilities. The reader
-    // itself doesn't need to care about frequency.
-    pub fn update_frequency(&mut self, next_frequency: f32) {
-        self.frequency = next_frequency;
-        self.wt_increment = 
-            freq_to_wt_inc(self.frequency, self.table_size, self.sample_rate);
+    // TODO: memoize frequency, sample_rate, and calculate phase inc from that.
+    pub fn increment(&mut self, freq: f32, sr: u32) {
+        if freq != self.memo.frequency || sr != self.memo.sample_rate {
+            self.memo.frequency = freq;
+            self.memo.sample_rate = sr;
+            self.memo.phase_inc = 1.0 / self.memo.sample_rate as f32 * self.memo.frequency
+        }
+        // While we could store the wavetable index and update it directly
+        // instead of using a normalized phase value, that would require us to
+        // make assumptions about the size of the wavetable, which we don't
+        // manage.
+        self.phase += self.memo.phase_inc;
+        // NOTE: The use of modulo on the audio thread could be dangerous. Find
+        // out and try to minimize this.
+        self.phase %= 1.0
     }
 }
-
-fn freq_to_phase_inc(freq: f32, sample_rate: u32) -> f32 {
-    1.0 / sample_rate as f32 * freq
-}
-
-// The OscReader doesn't track phase as a normalized value between 0 and 1. For
-// computational efficiency is tracks the actual wavetable index. This means that
-// the data type has to keep track on table size and stuff.
-fn freq_to_wt_inc(freq: f32, table_size: usize, sample_rate: u32) -> f32 {
-    table_size as f32 / sample_rate as f32 * freq
-}
-
