495e15adfc269c3b0decde763d822969edf93523
Kevin Waddle <bonzobavaria@gmail.com>
2020-06-28 14:05:25 -0500

    Simplify wavetable normalization

--- src/wavetable.rs
+++ src/wavetable.rs
@@ -15,11 +15,7 @@ pub fn make_sin_saw_table(table_size: usize, num_partials: usize) -> Vec<Wavetab
     make_wavetable(
         table_size,
         vec![
-            vec![Partial {
-                freq: 1.0,
-                amp: 1.0,
-                phase: 0.0,
-            }],
+            make_sine_partial(),
             make_triangle_partials(num_partials),
             make_square_partials(num_partials),
             make_sawtooth_partials(num_partials),
@@ -45,6 +41,14 @@ pub fn make_exp_envelope(table_size: usize, curve: f32) -> Wavetable {
 }
 
 // Partial generators
+
+fn make_sine_partial() -> Vec<Partial> {
+    vec![Partial {
+        freq: 1.0,
+        amp: 1.0,
+        phase: 0.0,
+    }]
+}
 
 fn make_triangle_partials(num_partials: usize) -> Vec<Partial> {
     // A triangle wave contains only odd harmonics, with alternating signs.
@@ -98,28 +102,19 @@ fn make_sawtooth_partials(num_partials: usize) -> Vec<Partial> {
 // normalizing the output vector so it's maximum absolute value is 1.0. These
 // operations are combined here for computational efficiency.
 pub fn make_fourier_table_norm(table_size: usize, partials: Vec<Partial>) -> Wavetable {
-    let mut wavetable: Vec<f32> = Vec::new();
     // Track maximum amplitude while creating wavetable, since we need to know
     // that to be able to normalize the wavetable.
-    let mut maximum_amplitude = 0.0;
+    let maximum_amplitude: f32 = partials.iter().map(|x| x.amp).sum();
+    let norm_factor: f32 = 1.0 / maximum_amplitude;
     let ts: f32 = 1.0 / table_size as f32;
+    let mut wavetable: Vec<f32> = Vec::new();
     for i in 0..table_size {
         let mut sample: f32 = 0.0;
         for partial in partials.iter() {
             let angle = TWO_PI * partial.freq * i as f32 * ts + partial.phase;
             sample += angle.sin() * partial.amp;
         }
-        if sample.abs() > maximum_amplitude {
-            maximum_amplitude = sample.abs();
-        }
-        wavetable.push(sample);
+        wavetable.push(sample * norm_factor);
     }
-    // Apply normalization to the generated wavetable by multiplying every
-    // sample by the normalization scale factor
-    let scale_factor: f32 = 1.0 / maximum_amplitude;
-    for sample in wavetable.iter_mut() {
-        *sample *= scale_factor;
-    }
-    // Return the generated wavetable
     wavetable
 }
